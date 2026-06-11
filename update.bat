f03ffb83f16fe3bc423cfdf8c97a727333663203
unknown <lurya.lur@gmail.com>
2026-06-11 04:14:12 -0300

    21

--- update.bat
+++ update.bat
@@ -1,14 +1,18 @@
 @echo off
 
-cd /d "%~dp0minecraft"
-
 echo Limpando mods...
-rmdir /s /q mods
+if exist mods rmdir /s /q mods
 
 echo Limpando config...
-rmdir /s /q config
+if exist config rmdir /s /q config
 
 echo Atualizando modpack...
 java -jar packwiz-installer-bootstrap.jar https://raw.githubusercontent.com/slxp7-4ny/modpackkkkkk/master/pack.toml
+
+echo.
+echo ========================================
+echo          Modpack atualizado!
+echo ========================================
+echo.
 
 pause

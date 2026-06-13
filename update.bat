b90c001801d5216b4be7d2d30544f6485a36587a
unknown <lurya.lur@gmail.com>
2026-06-13 07:05:57 -0300

    .

---
+++ update.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo Limpando mods...
+if exist mods rmdir /s /q mods
+
+echo Limpando config...
+if exist config rmdir /s /q config
+
+echo Atualizando modpack...
+java -jar packwiz-installer-bootstrap.jar https://raw.githubusercontent.com/slxp7-4ny/modpackkkkkk/master/pack.toml --bootstrap-no-update
+
+echo.
+echo ========================================
+echo          Modpack atualizado!
+echo ========================================
+echo.
+
+pause
